2539e16fb7ca1b833c8125e3d8cdba7ef49998c6
iamduy <duydkph10118@gmail.com>
2021-04-21 16:44:29 +0700

    last update

--- src/Asignment/game.ts
+++ src/Asignment/game.ts
@@ -1,47 +1,47 @@
 import _ from 'lodash'
 class game {
-    async pokemon(){
+    async pokemon() {
 
         const $ = document.querySelector.bind(document); //selector đến các element
 
         /* tạo 1 biến tính thời gian game chạy */
-        var count = 0; // tính time
+        var count = 120; // tính time
         const countdown = setInterval(() => {
-            count++;
-            if (count <= 300) {
+            count--;
+            if (count >= 0) {
                 $('#timeout').innerHTML = count;
             }
-            if (count === 300) {
+            if (count === 0) {
                 $('#result').innerHTML = 'Hết thời gian !';
                 $('#app').classList.add('hidden');
-              
+
             }
         }, 1000);
 
 
         /* form đăng nhập , ingame */
-        const form_login = /* html */`
-        <label class="block text-white">
-                Ingame <span class="text-red-400 text-sm">*</span>
-            <input type="text" id="ingame"
-            class="mt-1 p-2 border border-gray-100 block w-full rounded-md bg-gray-800 border-gray-700 text-white">
-        </label>
-        <button type="submit" class="w-full py-2 px-4 mt-6 text-center bg-white rounded-md text-black text-sm focus:outline-none"> Login </button>
-        `
-        $('#form').innerHTML = form_login;
+        // const form_login = /* html */`
+        // <label class="block text-white">
+        //         Ingame <span class="text-red-400 text-sm">*</span>
+        //     <input type="text" id="ingame"
+        //     class="mt-1 p-2 border border-gray-100 block w-full rounded-md bg-gray-800 border-gray-700 text-white">
+        // </label>
+        // <button type="submit" class="w-full py-2 px-4 mt-6 text-center bg-white rounded-md text-black text-sm focus:outline-none"> Login </button>
+        // `
+        // $('#form').innerHTML = form_login;
 
-        $('#form').addEventListener('submit', (e) => {
-            e.preventDefault();
-            if ($('#ingame').value === 'pokemon') {
-                console.log('login thành công');
-                clearInterval(countdown); // clear time
-                $('#main').classList.remove('hidden');
-                $('#form-login').classList.add('hidden');
-                this.pokemon();
-            } else {
-                $('#ingame').classList.add('border-red-400');
-            }
-        })
+        // $('#form').addEventListener('submit', (e) => {
+        //     e.preventDefault();
+        //     if ($('#ingame').value === 'pokemon') {
+        //         console.log('login thành công');
+        //         clearInterval(countdown); // clear time
+        //         $('#main').classList.remove('hidden');
+        //         $('#form-login').classList.add('hidden');
+        //         this.pokemon();
+        //     } else {
+        //         $('#ingame').classList.add('border-red-400');
+        //     }
+        // })
 
 
         /* Call api lấy dữ liệu */
@@ -54,7 +54,7 @@ class game {
 
         const myArr: types[] = []; //tạo biến là 1 mảng các phần tử đã đc định nghĩa kiểu dữ liệu
 
-        for (let i = 1; i <= 20; i++) {
+        for (let i = 1; i <= 12; i++) {
 
             //fetch data 
             const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
@@ -83,8 +83,8 @@ class game {
 
         const res = shuffle.map((items) => {
             return /* html */`
-            <div class="rounded-lg bg-white check-pokemon relative">
-                 <img data-id='${items.id}' class="mx-auto block" src="${items.image}">
+            <div class="rounded-lg bg-white check-pokemon relative w-20 h-28">
+                 <img data-id='${items.id}' src="${items.image}">
                  <div class="overlay remove bg-cover bg-center"></div>
             </div>
             
@@ -134,10 +134,10 @@ class game {
                                 item.classList.remove('choose', 'bg-white');
                                 item.classList.add('invisible');
                                 item.querySelector('img').remove();
-                                if (score === 200) {
+                                if (score === 120) {
                                     clearInterval(countdown);
                                     $('#app').classList.add('hidden');
-                                    $('#result').innerHTML = 'Bạn đã chiến thắng !';
+                                    $('#result').innerHTML = '<span>Bạn đã chiến thắng !</span>';
                                 }
                             }, 500)
                         })
